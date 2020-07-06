e2ecbfacee711d8027c3cd203105ef6114beaf79
Nirmalraaj <48831818+Nirmalraaj@users.noreply.github.com>
2020-07-06 14:58:45 +0530

    Add files via upload

---
+++ library.py
@@ -0,0 +1,36 @@
+import sys
+import datetime
+import pip
+import pkg_resources
+built_in_mod = sys.modules
+print(built_in_mod.keys())
+
+print("*****************************************************")
+'''if "sys" in built_in_mod:
+    print("true")'''
+#a=[datetime]
+#res=set(a)-set(built_in_mod)
+#print(list(res))
+#print(built_in_mod)
+pkgs = []
+pkgs1=[]
+#print([pip.main(['list'])])
+installed = [pkg.key for pkg in pkg_resources.working_set]
+print(installed)
+with open(r'F:\AWS\code\sample.py') as f:
+    for lines in f:
+        if "import" in lines and "#" not in lines:
+            tmp = lines.split("import")
+            #print(tmp[-1])
+            if tmp[-1].strip() not in built_in_mod and (tmp[-1].strip()).lower() not in installed:
+            #if tmp[-1].strip() not in built_in_mod:
+                    pkgs.append((tmp[-1].strip()))
+print(pkgs)
+print("+++++++++++++++++++++++++++++++++++++")
+#pkgs = set(pkgs)-set(installed)
+#print(list(pkgs))
+'''for package in pkgs:
+    if package in installed or package.lower() in installed:
+            pkgs.remove(package)
+            print(pkgs)'''
+#pip.main(['install'] + pkgs)
---
+++ sample.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import os
+import datetime
+import requests
+import awscli
+import botocore
+import boto3
+import mutagen
+import python-magic-bin
+import  werkzeug
+import sys
+#import pillow
+app = Flask(__name__)
+@app.route("/")
+def hello():
+   return "Hello from Python!"
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=5000)
